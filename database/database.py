ca31869b75663fd557b9616d74d71822509db2b6
Sandip-12da <169171803+Sandip-12da@users.noreply.github.com>
2024-12-22 21:00:24 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,5 +1,6 @@
-#(©)CodeXBotz
-
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+from config import DB_URI, DB_NAME, FILE_EXPIRATION_TIME, MAX_FILE_DOWNLOADS
 
 
 
@@ -13,7 +14,19 @@ database = dbclient[DB_NAME]
 
 user_data = database['users']
 
+async def add_file_with_expiry(file_id, expiration_time=FILE_EXPIRATION_TIME):
+    expiry = datetime.utcnow() + timedelta(seconds=expiration_time)
+    file_data.update_one(
+        {'_id': file_id},
+        {'$set': {'expiry_time': expiry}},
+        upsert=True
+    )
 
+async def get_expired_files():
+    current_time = datetime.utcnow()
+    return list(file_data.find({
+        'expiry_time': {'$lt': current_time}
+    }))
 
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
